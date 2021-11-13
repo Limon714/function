9ba834ec1e3ad24e439710cbd405912cc7f0e37e
Abdul Khalaque <72975868+Limon714@users.noreply.github.com>
2021-11-13 23:01:49 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+function learnPhp(){
+
+    echo " Hi, We learn PHP Function" . "<br>";
+
+}
+
+learnPhp();
+learnPhp();
+learnPhp();
+echo "PHP is interesting <br>";
+learnPhp();
+learnPhp();
+
+?>
