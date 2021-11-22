e501ac5c17a9587097ef0737a7c4d5e9d69df733
Abdul Khalaque <72975868+Limon714@users.noreply.github.com>
2021-11-23 02:08:17 +0600

    Update traversaryFunction.php

--- traversaryFunction.php
+++ traversaryFunction.php
@@ -2,13 +2,19 @@
 
 $food = array('orange', 'banana', 'apple', 'grapes');
 
-echo current($food);
-echo key($food);
-echo pos($food);
+echo current($food) . "<br>";
+echo "<b> Key :</b>" . key($food)  . "<br>";
+echo pos($food). "<br>";
 
 next($food);
-echo "<b> Current :</b>" .current($food);
+echo "<b> Current :</b>" .current($food). "<br>";
 next($food);
+echo "<b> Current :</b>" .current($food) . "<br>";
+prev($food);
 echo "<b> Current :</b>" .current($food);
+echo key($food) . "<br>";
+reset($food);
+echo "<b> Current :</b>" .current($food);
+echo key($food);
 
 ?>
